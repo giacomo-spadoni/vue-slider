cea0a6b4f271f6f60317a7c47b666b2577e3bb1b
giacomo-spadoni <giaky.spado@gmail.com>
2024-05-13 22:05:14 +0200

    added timer bonus

--- js/script.js
+++ js/script.js
@@ -1,29 +1,3 @@
-const slides = [
-    {
-        image: 'img/01.webp',
-        title: 'Marvel\'s Spiderman Miles Morale',
-        text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
-    }, {
-        image: 'img/02.webp',
-        title: 'Ratchet & Clank: Rift Apart',
-        text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
-    }, {
-        image: 'img/03.webp',
-        title: 'Fortnite',
-        text: "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
-    }, {
-        image: 'img/04.webp',
-        title: 'Stray',
-        text: 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
-    }, {
-        image: 'img/05.webp',
-        title: "Marvel's Avengers",
-        text: 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
-    }
-];
-
-console.log(slides);
-
 const { createApp } = Vue
 
 createApp({
@@ -50,7 +24,8 @@ createApp({
                 'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
                 'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
             ],
-            activeImage: 0
+            activeImage: 0,
+            timer: '',
         }
     },
     methods: {
@@ -63,6 +38,11 @@ createApp({
             } else {
                 this.activeImage++
             }
+            clearInterval(this.timer)
+            this.timer = setInterval(() => {
+                this.next();
+                console.log(this.activeImage)
+            }, 3000)
         },
         prev() {
             if (this.activeImage == 0) {
@@ -70,6 +50,11 @@ createApp({
             } else {
                 this.activeImage--
             }
+            clearInterval(this.timer)
+            this.timer = setInterval(() => {
+                this.next();
+                console.log(this.activeImage)
+            }, 3000)
         },
         show(i) {
             if (i == this.activeImage) {
@@ -79,5 +64,11 @@ createApp({
             }
 
         }
+    },
+    mounted() {
+        this.timer = setInterval(() => {
+            this.next();
+            console.log(this.activeImage)
+        }, 3000)
     }
 }).mount('#app')

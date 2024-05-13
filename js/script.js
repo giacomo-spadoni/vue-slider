b4b09863ddb6cd48ce265ac193f8fdff474e5453
giacomo-spadoni <giaky.spado@gmail.com>
2024-05-13 21:51:47 +0200

    Milestones complited

--- js/script.js
+++ js/script.js
@@ -36,6 +36,20 @@ createApp({
                 'img/04.webp',
                 'img/05.webp',
             ],
+            title: [
+                'Marvel\'s Spiderman Miles Morale',
+                'Ratchet & Clank: Rift Apart',
+                'Fortnite',
+                'Stray',
+                "Marvel's Avengers",
+            ],
+            text: [
+                'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
+                'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
+                "Grab all of your friends and drop into Epic Games Fortnite, a massive 100 - player face - off that combines looting, crafting, shootouts and chaos.",
+                'Lost, injured and alone, a stray cat must untangle an ancient mystery to escape a long-forgotten city',
+                'Marvel\'s Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.',
+            ],
             activeImage: 0
         }
     },
@@ -56,6 +70,14 @@ createApp({
             } else {
                 this.activeImage--
             }
+        },
+        show(i) {
+            if (i == this.activeImage) {
+                return true
+            } else {
+                return false
+            }
+
         }
     }
 }).mount('#app')
